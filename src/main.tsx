a4df8bdaa347747f6dae826a50be36cef56072d8
unknown <soulmamoudou0@gmail.com>
2025-10-27 11:50:23 +0000

    fix: routing for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
-// main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Toaster } from "react-hot-toast";
 
-const rootElement = document.getElementById('root')
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-  )
-} else {
-  console.error('Root element not found')
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Toaster position="top-center" reverseOrder={false}/>
+    <div className='flex justify-center items-center min-h-screen my-5'>
+       <App />
+    </div>
+  </StrictMode>,
+)
