134fcf0093e8d2b9c28f694a328f4f6e7ef658e0
unknown <soulmamoudou0@gmail.com>
2025-10-27 12:18:32 +0000

    fix: routing for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Toaster } from "react-hot-toast";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App.jsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Toaster position="top-center" reverseOrder={false}/>
-    <div className='flex justify-center items-center min-h-screen my-5'>
-       <App />
-    </div>
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+  )
+}
